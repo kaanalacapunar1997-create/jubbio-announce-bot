39d02e16cc86c18e2c0e43deea28e1d869c03cf0
kaan alacapunar <kaan.alacapunar1997@gmail.com>
2026-02-22 14:21:15 +0300

    clean play.js

--- commands/play.js
+++ commands/play.js
@@ -1,38 +1,68 @@
 const { spawn } = require("child_process");
+const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require("@jubbio/voice");
 const path = require("path");
+const fs = require("fs");
 
-const filePath = path.join(__dirname, "song.mp3");
+module.exports = {
+  name: "play",
+  async execute(client, message, args) {
 
-const ytdlp = spawn("yt-dlp", [
-  "-x",
-  "--audio-format",
-  "mp3",
-  "-o",
-  filePath,
-  url
-]);
+    const voiceChannelId = message.member?.voice?.channel_id;
+    if (!voiceChannelId) {
+      return message.reply("Odaya gir.");
+    }
 
-ytdlp.stdout.on("data", data => {
-  console.log("YTDLP STDOUT:", data.toString());
-});
+    const url = args[0];
+    if (!url) {
+      return message.reply("Bir YouTube linki gir.");
+    }
 
-ytdlp.stderr.on("data", data => {
-  console.log("YTDLP STDERR:", data.toString());
-});
+    message.reply("İndiriliyor...");
 
-ytdlp.on("error", err => {
-  console.log("SPAWN ERROR:", err);
-});
+    const filePath = path.join(__dirname, "song.mp3");
 
-ytdlp.on("close", async (code) => {
-  console.log("YTDLP CLOSED WITH CODE:", code);
+    // eski dosya varsa sil
+    if (fs.existsSync(filePath)) {
+      fs.unlinkSync(filePath);
+    }
 
-  if (code !== 0) {
-    return message.reply("❌ İndirme başarısız.");
+    const ytdlp = spawn("yt-dlp", [
+      "-x",
+      "--audio-format",
+      "mp3",
+      "-o",
+      filePath,
+      url
+    ]);
+
+    ytdlp.stderr.on("data", data => {
+      console.log("YTDLP:", data.toString());
+    });
+
+    ytdlp.on("error", err => {
+      console.log("SPAWN ERROR:", err);
+    });
+
+    ytdlp.on("close", async (code) => {
+      console.log("YTDLP EXIT CODE:", code);
+
+      if (code !== 0) {
+        return message.reply("❌ İndirme başarısız.");
+      }
+
+      const connection = joinVoiceChannel({
+        channelId: voiceChannelId,
+        guildId: message.guild_id,
+        adapterCreator: message.guild.voiceAdapterCreator
+      });
+
+      const player = createAudioPlayer();
+      const resource = createAudioResource(filePath);
+
+      player.play(resource);
+      connection.subscribe(player);
+
+      message.reply("Çalıyor...");
+    });
   }
-
-  message.reply("Çalıyor...");
-
-  const resource = createAudioResource(filePath);
-  player.play(resource);
-});
+};
